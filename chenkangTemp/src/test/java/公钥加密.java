5bacd1af205f1601ebdefb4e756223a1b8b15247
chenkang <741635037@qq.com>
2021-04-09 12:37:20 +0800

    11111111111

--- chenkangTemp/src/test/java/公钥加密.java
+++ chenkangTemp/src/test/java/公钥加密.java
@@ -8,17 +8,19 @@ import java.util.Random;
 
 public class 公钥加密 {
     public static void main(String[] arg) {
-//        PublicKey{point1=DemoPoint{x=0, y=1}, point2=DemoPoint{x=72176107556, y=4368835808}}
-        PublicKey publicKey = new PublicKey(new DemoPoint("0", "1"), new DemoPoint("72176107556", "4368835808"));//这是别人发给我们的公钥
+//      PublicKey{point1=DemoPoint{x=0, y=1}, point2=DemoPoint{x=61745727438, y=19524854285}}
+        PublicKey publicKey = new PublicKey(new DemoPoint("0", "1"), new DemoPoint("61745727438", "19524854285"));//这是别人发给我们的公钥
 
-//      假如我要告诉别人一个数字  2020
-        Integer rand = new Random().nextInt();//先随便产生一个数字,写死也可以,不影响
+//      假如我要告诉别人一个数字 77777777777
 
-        //用公钥的2个数字分别加入rand这个随机的数字
+        Long rand = new Random().nextLong();//先随便产生一个数字越大越好,这个数字越大越不好破解
+        //用参数的数字和公钥的2个点运算
         Point point1 = EccUtil.NumberMultiplication(publicKey.point1, new BigInteger(rand + ""));
         Point point2 = EccUtil.NumberMultiplication(publicKey.point2, new BigInteger(rand + ""));
-        point2 = point2.encode(new BigInteger("2020"));//2020是我们要发送的数据
+        //然后使用你偏移量来作为我们要发送的数字,这里假如我们要发送的数字是2020
+        point2 = point2.encode(new BigInteger("2020"));
 
         System.out.println(point1 + "  " + point2);//加密好以后把point1和point2 发送过去
+        //DemoPoint{x=19075198770, y=12493183992}  DemoPoint{x=89852588610, y=29392771030}
     }
 }
--- chenkangTemp/src/test/java/私钥解密.java
+++ chenkangTemp/src/test/java/私钥解密.java
@@ -5,14 +5,14 @@ import java.math.BigInteger;
 
 public class 私钥解密 {
     public static void main(String[] arg) {
-        PrivateKey rivateKey = new PrivateKey(new DemoPoint("0", "1"), new BigInteger("9876543210"));//我们自己的私钥
-//      这是别人用公钥加密好的密文
-//      DemoPoint{x=49967128010, y=55727664622}  DemoPoint{x=50328711320, y=71399355806}
-        Point p1 = new DemoPoint("49967128010", "55727664622");
-        Point p2 = new DemoPoint("50328711320", "71399355806");
+        PrivateKey rivateKey = new PrivateKey(new DemoPoint("0", "1"), new BigInteger("9999999999999"));
 
-        p1 = EccUtil.NumberMultiplication(p1,new BigInteger("9876543210"));
-        System.out.println(p2.decode(p1));
+        //      这是别人用公钥加密好的密文
+        //DemoPoint{x=19075198770, y=12493183992}  DemoPoint{x=89852588610, y=29392771030}
+        Point p1 = new DemoPoint("19075198770", "12493183992");
+        Point p2 = new DemoPoint("89852588610", "29392771030");
 
+        p1 = EccUtil.NumberMultiplication(p1, rivateKey.getK());
+        System.out.println("私钥解密出来的是:" + p2.decode(p1));
     }
 }
