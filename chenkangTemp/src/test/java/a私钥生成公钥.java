96fd6620354366052210fb204db87269c87953a4
chenkang <741635037@qq.com>
2021-04-09 12:38:59 +0800

    11111111111

--- chenkangTemp/src/test/java/a私钥生成公钥.java
+++ chenkangTemp/src/test/java/a私钥生成公钥.java
@@ -3,7 +3,7 @@ import MyEcc.DemoPoint;
 
 import java.math.BigInteger;
 
-public class 私钥生成公钥 {
+public class a私钥生成公钥 {
     public static void main(String[] arg) {
         //自己做一个私钥,随便选一个点,随便选一个大数越大越好 这里做例子就选点(0,1) 和 9999999999999 作为私钥
         PrivateKey rivateKey = new PrivateKey(new DemoPoint("0", "1"), new BigInteger("9999999999999"));
